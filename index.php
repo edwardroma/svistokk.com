d54f982e669a22bbd9096419fcaebae03dc164a3
EdwardR <freelance_7@yahoo.com>
2019-09-14 20:27:26 +0300

    index.php 1

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-echo "Welcome to SqyNet's 1st app"; 
+session_start();
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
 
+echo $_SERVER['PATH_INFO'];
+die();
+
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
 
 ?>

41394c3ba7c0e3c138956a624e482210e35b359a
EdwardR <freelance_7@yahoo.com>
2019-09-17 00:06:06 +0300

    supporting .php files

---
+++ core/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+// load config and helper
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+// Autoload classes
+function __autoload($classname) {
+
+if(file_exists(ROOT . DS . 'core' . DS . $classname . '.php')) {
+  require_once(ROOT . DS . 'core' . DS . $classname . '.php');
+} elseif (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $classname . '.php')) {
+  require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $classname . '.php');
+} elseif (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $classname . '.php')) {
+  require_once(ROOT . DS . 'app' . DS . 'models' . DS . $classname . '.php');
+	}
+}
+
+//Route the request
+Router::route($url);
--- index.php
+++ index.php
@@ -4,9 +4,8 @@ session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 
-echo $_SERVER['PATH_INFO'];
-die();
-
 $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
 
 ?>
